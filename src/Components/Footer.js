6ac63854d308f2f676eb03fdf475a80c176f2734
Simeon Akanbi <84454184+simple-codes22@users.noreply.github.com>
2021-12-29 16:01:17 +0100

    Testing

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -101,7 +101,7 @@ const Footer = () => {
                         <Typography variant='body2' className={useStyle.navCover}><a className={useStyle.navLinks} href='#stacks'>Stacks</a></Typography>
                         <Typography variant='body2' className={useStyle.navCover}><a className={useStyle.navLinks} href='#projects'>Projects</a></Typography>
                         <Typography variant='body2' className={useStyle.navCover}><a className={useStyle.navLinks} href='#contact'>Contact me</a></Typography>
-                        <Button href='' variant='contained' style={{
+                        <Button href='https://witsodqqjbrlgpnxvsra.supabase.in/storage/v1/object/sign/resume-bucket/My Resume.pdf?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJyZXN1bWUtYnVja2V0L015IFJlc3VtZS5wZGYiLCJpYXQiOjE2NDA3ODk5NTUsImV4cCI6MTk1NjE0OTk1NX0.-gV8KjxKGeIfAB6Rd0KBM2UJs7eu_AKzmpTIzyrEGck' variant='contained' style={{
                             height: '32px !important',
                             width: '88px !important',
                             marginLeft: '9px !important',
