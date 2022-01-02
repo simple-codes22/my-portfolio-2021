81f80b68a34532575880f4e37fbf5785dadcc71b
Simeon Akanbi <84454184+simple-codes22@users.noreply.github.com>
2022-01-02 22:10:12 +0100

    Added my social media links

--- src/Components/Home/misc/Social.js
+++ src/Components/Home/misc/Social.js
@@ -45,9 +45,9 @@ const Social = () => {
     const useStyle = socialStyle();
     return (
         <Box component='div' className={useStyle.root}>
-                <a href='https://www.github.com/' target='_blank' rel="noreferrer" className={`${useStyle.iconSpace} ${useStyle.GitHubIcon}`}><GitHub /></a>
-                <a href='https://www.linkedin.com/' target='_blank' rel="noreferrer" className={`${useStyle.iconSpace}  ${useStyle.LinkedInIcon}`}><LinkedIn /></a>
-                <a href='https://www.twitter.com/' target='_blank' rel="noreferrer" className={`${useStyle.iconSpace}  ${useStyle.TwitterIcon}`}><Twitter /></a>
+                <a href='https://www.github.com/simple-codes22' target='_blank' rel="noreferrer" className={`${useStyle.iconSpace} ${useStyle.GitHubIcon}`}><GitHub /></a>
+                <a href='https://www.linkedin.com/simple-codes22' target='_blank' rel="noreferrer" className={`${useStyle.iconSpace}  ${useStyle.LinkedInIcon}`}><LinkedIn /></a>
+                <a href='https://www.twitter.com/simple-codes' target='_blank' rel="noreferrer" className={`${useStyle.iconSpace}  ${useStyle.TwitterIcon}`}><Twitter /></a>
                 <a href='https://www.facebook.com/' target='_blank' rel="noreferrer" className={`${useStyle.iconSpace}  ${useStyle.FacebookIcon}`}><Facebook /></a>
         </Box>
     )
