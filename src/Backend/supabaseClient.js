3c43853ae04128534c0b478a97b7c7f8383fa5df
Simeon Akanbi <84454184+simple-codes22@users.noreply.github.com>
2022-04-26 22:33:47 +0100

    Update supabaseClient.js

--- src/Backend/supabaseClient.js
+++ src/Backend/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://witsodqqjbrlgpnxvsra.supabase.co'
+const supabaseUrl = process.env.REACT_APP_SUPABASE_PROVIDER;
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
-export const resumeUrl = `https://witsodqqjbrlgpnxvsra.supabase.in/storage/v1/object/sign/resume-bucket/My Resume.pdf?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJyZXN1bWUtYnVja2V0L015IFJlc3VtZS5wZGYiLCJpYXQiOjE2NDA3ODk5NTUsImV4cCI6MTk1NjE0OTk1NX0.-gV8KjxKGeIfAB6Rd0KBM2UJs7eu_AKzmpTIzyrEGck`;
+export const resumeUrl = process.env.REACT_APP_RESUME
 
 
